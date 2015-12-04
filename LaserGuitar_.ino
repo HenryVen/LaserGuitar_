e247a67c7c4a9ab0d908b3c4f7a9febde6f17c98
HenryVen <heve9547@colorado.edu>
2015-12-03 17:41:08 -0700

    Update LaserGuitar_.ino

--- LaserGuitar_.ino
+++ LaserGuitar_.ino
@@ -1,6 +1,6 @@
 #include <SPI.h>
 #include <SdFat.h>
-#include <SdFatUtil.h> 
+#include <SdFatUtil.h>
 #include <SFEMP3Shield.h>
 #define inPin0 0
 #define inPin1 1
@@ -8,23 +8,26 @@
 #define inPin3 3
 #define inPin4 4
 #define inPin5 5
-//#define debug
+#define debug
 
 SdFat sd;
 SFEMP3Shield MP3player;
 volatile int state = 0;
-const int buttonPin = 2;    
-int buttonState = 0;       
-
+const int buttonPin = 2;
+int pinStates [6] = {0, 0, 0, 0, 0, 0};
+int cutoff = 60;
+int lasttrack = -999;
+// Read Pins, store into Array
+int repeat = 1;
 
 void setup(void) {
-  attachInterrupt(digitalPinToInterrupt(buttonPin),buttonpress, FALLING);
- #ifdef debug
+#ifdef debug
   Serial.begin(9600);
   Serial.println();
-  #endif
+#endif
   sd.begin(SD_SEL, SPI_HALF_SPEED);
   MP3player.begin();
+
   pinMode(buttonPin, INPUT);
   pinMode(inPin0, INPUT);
   pinMode(inPin1, INPUT);
@@ -32,128 +35,63 @@ void setup(void) {
   pinMode(inPin3, INPUT);
   pinMode(inPin4, INPUT);
   pinMode(inPin5, INPUT);
+  pinMode(5, INPUT);
 
-  
 }
- 
+
 void loop(void) {
- 
-  int pinRead0 = analogRead(inPin0);
-  int pinRead1 = analogRead(inPin1);
-  int pinRead2 = analogRead(inPin2);
-  int pinRead3 = analogRead(inPin3);
-  int pinRead4 = analogRead(inPin4);
-  int pinRead5 = analogRead(inPin5);
- 
- #ifdef debug
-  Serial.print(pinRead0);
-  Serial.print("  ");
-  Serial.print(pinRead1);
-  Serial.print("  ");
-  Serial.print(pinRead2);
-  Serial.print("  ");
-  Serial.print(pinRead3);
-  Serial.print("  ");
-  Serial.print(pinRead4);
-  Serial.print("  ");
-  Serial.print(pinRead5);
-  Serial.print("  ");
+
+
+  if (digitalRead(5) == HIGH)
+  {
+    state = state + 1;
+
+
+    if (state > 3)
+    {
+      state = 0;
+    }
+  }
+
+  for (int j = 0; j < 6; j++) {
+    pinStates[j] = analogRead(j);
+  }
+  for (int i = 0; i < 6; i++) {
+    if (pinStates[i] < cutoff && repeat == 1) {
+      MP3player.stopTrack();
+      MP3player.playTrack(6 * state + (i + 1));
+      lasttrack = i;
+      repeat = 0;
+    }
+    if (!MP3player.isPlaying())
+    {
+      lasttrack = -999;
+    }
+    if (MP3player.isPlaying() && analogRead(i) > cutoff )
+    {
+      repeat = 1;
+    }
+  }
+
+
+
+#ifdef debug
+  for (int k = 0; k < 6; k++) {
+    Serial.print(pinStates[k]);
+    Serial.print(" ");
+  }
   Serial.println(state);
   delay(200);
-  #endif
-  
-//int state = 0 define state?
-  
-  buttonState = digitalRead(buttonPin);
-
-
-   switch (state) {
-    //state= case #?
- 
-    case 1:
-    
-   if (pinRead0 < 50)
-  {
-    MP3player.playTrack(1);
-  
-  }
-  if (pinRead1 < 50)
-  {
-     MP3player.playTrack(2);
-  }  
-   if (pinRead2 < 50)
-  {
-     MP3player.playTrack(3);
-  
-  }  
- if (pinRead3 < 50)
-  {
-     MP3player.playTrack(4);
-  
-  }  
- if (pinRead4 < 50)
-  {
-     MP3player.playTrack(5);
-  
-  }  
- if (pinRead5 < 50)
-  {
-     MP3player.playTrack(6);
-  }  
-      //do something when state equals 1
-      break;
-    case 2:
-
-     if (pinRead0 > 50)
-  {
-    MP3player.playTrack(7);
-  
-  }
-  if (pinRead1 > 10)
-  {
-     MP3player.playTrack(8);
-  }  
-   if (pinRead2 > 10)
-  {
-     MP3player.playTrack(9);
-  
-  }  
- if (pinRead3 > 10)
-  {
-     MP3player.playTrack(10);
-  
-  }  
- if (pinRead4 > 10)
-  {
-     MP3player.playTrack(11);
-  
-  }  
- if (pinRead5 > 10)
-  {
-     MP3player.playTrack(12);
-  }  
-      //do something when state equals 2
-      break;
-    default: 
-     Serial.println("error");
- //set case to 1
-      // if nothing else matches, do the default
-      // default is optional
-    break;
-  }
-  
- if (state>2)
- {
-  state = 1;
- }
-
+#endif
 
 
 }
 
-   void buttonpress() {
-  state++; 
-   }
+void buttonpress() {
+  state++;
+  if (state > 2)
+  {
+    state = 0;
+  }
 
-  
-
+}
